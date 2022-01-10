0772f3aeb124fa8d6ca2531e8830052ad5d29adf
Suryanto <suryanto@grip-principle.com>
2022-01-11 00:29:43 +0700

    Looping leeper content using map() function

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Components/header";
 import Footer from "./Components/footer";
 import Note from "./Components/note";
+import notes from "./notes";
+
+function CreateCard(notes) {
+    return (
+    <Note
+    key= {notes.id} 
+    title = {notes.title} 
+    content={notes.content}/>)
+}
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
+            {notes.map((CreateCard))}
             <Footer />
         </div>
     );
